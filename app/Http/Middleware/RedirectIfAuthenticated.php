17f531eadbe0e794da6a60a0ab2224d1d3ed95c7
Basma Farouk mohamd <basmafarouk05@gamil.com>
2025-12-11 14:40:03 +0200

    handle register forgot and reset pages

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -39,7 +39,7 @@ class RedirectIfAuthenticated
     /**
      * Get the path the user should be redirected to when they are authenticated.
      */
-    protected function redirectTo(Request $request, string $guard = 'web'): ?string
+    protected function redirectTo(Request $request, ?string $guard = 'web'): ?string
     {
         return static::$redirectToCallback
             ? call_user_func(static::$redirectToCallback, $request)
--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,43 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+
+@extends('frontend.layouts.app')
+
+@section('contents')
+    <x-frontend.breadcrumb :items="[
+        ['label' => 'Home', 'url' => '/'],
+        ['label' => 'Reset Password'],
+    ]" />
+
+    <div class="page-content pt-150 pb-140">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-5 col-lg-6 col-md-12 m-auto">
+                    <x-auth-session-status class="mb-4" :status="session('status')" />
+                    <div class="login_wrap widget-taber-content background-white">
+                        <div class="padding_eight_all bg-white">
+                            <div class="heading_s1">
+                                <img class="border-radius-15" src="assets/imgs/page/forgot_password.svg" alt="" />
+                                <h2 class="mb-15 mt-15">Forgot your password?</h2>
+                                <p class="mb-30">Not to worry, we got you! Let’s get you a new password. Please
+                                    enter your email address or your Username.</p>
+                            </div>
+                            <form method="post" action="{{ route('password.email') }}">
+                                @csrf
+                                <div class="form-group">
+                                    <input type="email" required="" name="email" placeholder="Email *" value="{{ old('email') }}"/>
+                                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                                </div>
+
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-heading btn-block hover-up" name="login">Reset
+                                        password</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+@endsection
